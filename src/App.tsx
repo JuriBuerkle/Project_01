9d15e76f674712750c8cf6c2c6a18435950722c2
Juri Buerkle <rastopka@gmx.de>
2025-09-10 07:16:51 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import AgeInfo from './components/AgeInfo/AgeInfo';
+import Goodbye from './components/Goodbye/Goodbye'
+import Greeting from './components/Greeting/Greeting'
+
+function App() {
+  return (
+    <div>
+      {/* <p>Hello, React !</p> */}
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Goodbye name={"Bob"} />
+      <Goodbye name={"John"} />
+      <AgeInfo age={23} name={"Andrey"} />
+      <AgeInfo age={26} name={"Fred"} />
+    </div>
+
+  );
+}
+
+export default App
+
